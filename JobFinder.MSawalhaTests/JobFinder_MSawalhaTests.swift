d31f9c0e5806d6ffb6c1694c783b6720e38bbfdd
mohmmad sawalha <mohmmadsawalha321@gmail.com>
2019-03-30 16:41:59 +0300

    Initial Commit

---
+++ JobFinder.MSawalhaTests/JobFinder_MSawalhaTests.swift
@@ -0,0 +1,34 @@
+//
+//  JobFinder_MSawalhaTests.swift
+//  JobFinder.MSawalhaTests
+//
+//  Created by apple on 3/30/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import XCTest
+@testable import JobFinder_MSawalha
+
+class JobFinder_MSawalhaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
