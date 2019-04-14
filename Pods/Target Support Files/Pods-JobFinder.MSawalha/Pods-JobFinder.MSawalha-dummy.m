427df915aafbc8be273580460a9ebb7b5eaaf892
mohmmad sawalha <mohmmadsawalha321@gmail.com>
2019-04-14 18:59:22 +0300

    added pod file

---
+++ Pods/Target Support Files/Pods-JobFinder.MSawalha/Pods-JobFinder.MSawalha-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JobFinder_MSawalha : NSObject
+@end
+@implementation PodsDummy_Pods_JobFinder_MSawalha
+@end
---
+++ Pods/Target Support Files/Pods-JobFinder.MSawalhaTests/Pods-JobFinder.MSawalhaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JobFinder_MSawalhaTests : NSObject
+@end
+@implementation PodsDummy_Pods_JobFinder_MSawalhaTests
+@end
